920cfe91b80c5dc7a6e6675ac0bc298981e2c3f2
Chris Monson <shiblon@gmail.com>
2013-09-10 19:29:38 -0400

    Added topology

--- pso/swarm.go
+++ pso/swarm.go
@@ -1,12 +1,13 @@
 package pso
 
 type Swarm struct {
-	Dim       int
-	Vdim	  int
-	Particles []*Particle
+	Dim          int
+	Vdim         int
+	Particles    []*Particle
+	Neighborhood func(int) []int
 }
 
-func NewSwarm(dim, vdim int, size int) (swarm *Swarm) {
+func NewSwarm(dim, vdim int, size int, neighborhood func(int) []int) (swarm *Swarm) {
 	swarm = new(Swarm)
 	swarm.Dim = dim
 	swarm.Vdim = vdim
