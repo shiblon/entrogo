47b3749f50519d8b93a02ef12c719c1aa40ceb35
Chris Monson <shiblon@gmail.com>
2014-02-18 17:21:19 -0500

    Not completed, but a reasonable task queue implementation is working.

---
+++ taskstore/pqueue_test.go
@@ -0,0 +1,209 @@
+// Copyright 2014 Chris Monson <shiblon@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package taskstore
+
+import (
+	"fmt"
+)
+
+var (
+	BasicTasks = []*Task{
+		{
+			ID:    1,
+			AT:    1000,
+			Owner: 1,
+			Group: "group1",
+			Data:  "data1",
+		},
+		{
+			ID:    2,
+			AT:    1004,
+			Owner: 1,
+			Group: "group1",
+			Data:  "data2",
+		},
+		{
+			ID:    3,
+			AT:    999,
+			Owner: 1,
+			Group: "group1",
+			Data:  "data3",
+		},
+		{
+			ID:    4,
+			AT:    1005,
+			Owner: 1,
+			Group: "group1",
+			Data:  "data4",
+		},
+		{
+			ID:    5,
+			AT:    1002,
+			Owner: 1,
+			Group: "group1",
+			Data:  "data5",
+		},
+		{
+			ID:    6,
+			AT:    1001,
+			Owner: 1,
+			Group: "group1",
+			Data:  "data6",
+		},
+		{
+			ID:    7,
+			AT:    1003,
+			Owner: 1,
+			Group: "group1",
+			Data:  "data7",
+		},
+	}
+)
+
+func Example_newTaskQueue() {
+	tq := NewTaskQueue("group1")
+	fmt.Println(tq.Name, tq.taskHeap, tq.taskMap, cap(tq.randChan))
+
+	// Output:
+	// group1 [] map[] 1
+}
+
+func Example_newTaskQueueFromTasks() {
+	tq := NewTaskQueueFromTasks("group1", BasicTasks)
+
+	fmt.Println(tq)
+
+	// Output:
+	//
+	// TQ name=group1
+	//    heap=[
+	//       {0:Task 3: group=group1 owner=1 at=999 data="data3"}
+	//       {1:Task 5: group=group1 owner=1 at=1002 data="data5"}
+	//       {2:Task 1: group=group1 owner=1 at=1000 data="data1"}
+	//       {3:Task 4: group=group1 owner=1 at=1005 data="data4"}
+	//       {4:Task 2: group=group1 owner=1 at=1004 data="data2"}
+	//       {5:Task 6: group=group1 owner=1 at=1001 data="data6"}
+	//       {6:Task 7: group=group1 owner=1 at=1003 data="data7"}
+	//    ]
+	//    map=[
+	//       ID 1 = index 2
+	//       ID 2 = index 4
+	//       ID 3 = index 0
+	//       ID 4 = index 3
+	//       ID 5 = index 1
+	//       ID 6 = index 5
+	//       ID 7 = index 6
+	//    ]
+	//    chancap=1
+}
+
+func Example_push() {
+	tq := NewTaskQueueFromTasks("group1", BasicTasks)
+	tq.Push(&Task{
+		ID:    7,
+		AT:    998,
+		Group: "group1",
+		Data:  nil,
+	})
+
+	fmt.Println(tq)
+
+	// Output:
+	//
+	// TQ name=group1
+	//    heap=[
+	//       {0:Task 7: group=group1 owner=0 at=998 data=<nil>}
+	//       {1:Task 3: group=group1 owner=1 at=999 data="data3"}
+	//       {2:Task 1: group=group1 owner=1 at=1000 data="data1"}
+	//       {3:Task 5: group=group1 owner=1 at=1002 data="data5"}
+	//       {4:Task 2: group=group1 owner=1 at=1004 data="data2"}
+	//       {5:Task 6: group=group1 owner=1 at=1001 data="data6"}
+	//       {6:Task 7: group=group1 owner=1 at=1003 data="data7"}
+	//       {7:Task 4: group=group1 owner=1 at=1005 data="data4"}
+	//    ]
+	//    map=[
+	//       ID 1 = index 2
+	//       ID 2 = index 4
+	//       ID 3 = index 1
+	//       ID 4 = index 7
+	//       ID 5 = index 3
+	//       ID 6 = index 5
+	//       ID 7 = index 0
+	//    ]
+	//    chancap=1
+}
+
+func Example_pop() {
+	tq := NewTaskQueueFromTasks("group1", BasicTasks)
+
+	// Pop the oldest task (the lowest Available Time)
+	task := tq.Pop()
+
+	fmt.Println(task)
+
+	// Output:
+	// Task 3: group=group1 owner=1 at=999 data="data3"
+}
+
+func Example_popAt() {
+	tq := NewTaskQueueFromTasks("group1", BasicTasks)
+
+	// Pop a task from the middle
+	task := tq.PopAt(4)
+
+	// Note that we get the proper task, and the queue is reorganized to fit
+	// the new situation.
+	fmt.Println(task)
+	fmt.Println(tq)
+
+	// Output:
+	// Task 2: group=group1 owner=1 at=1004 data="data2"
+	// TQ name=group1
+	//    heap=[
+	//       {0:Task 3: group=group1 owner=1 at=999 data="data3"}
+	//       {1:Task 5: group=group1 owner=1 at=1002 data="data5"}
+	//       {2:Task 1: group=group1 owner=1 at=1000 data="data1"}
+	//       {3:Task 4: group=group1 owner=1 at=1005 data="data4"}
+	//       {4:Task 7: group=group1 owner=1 at=1003 data="data7"}
+	//       {5:Task 6: group=group1 owner=1 at=1001 data="data6"}
+	//    ]
+	//    map=[
+	//       ID 1 = index 2
+	//       ID 3 = index 0
+	//       ID 4 = index 3
+	//       ID 5 = index 1
+	//       ID 6 = index 5
+	//       ID 7 = index 4
+	//    ]
+	//    chancap=1
+}
+
+func Example_Peek() {
+	tq := NewTaskQueueFromTasks("group1", BasicTasks)
+
+	fmt.Println(tq.Peek())
+
+	// Output:
+	// Task 3: group=group1 owner=1 at=999 data="data3"
+}
+
+func Example_peekAt() {
+	tq := NewTaskQueueFromTasks("group1", BasicTasks)
+
+	fmt.Println(tq.PeekAt(3))
+
+	// Output:
+	// Task 4: group=group1 owner=1 at=1005 data="data4"
+}
---
+++ taskstore/taskstore.go
@@ -0,0 +1,144 @@
+// Copyright 2014 Chris Monson <shiblon@gmail.com>
+//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+Package taskstore implements a library for a simple task store.
+
+This provides abstractions for creating a simple task store process that
+manages data in memory and on disk. It can be used to implement a full-fledged
+task queue, but it is only the core storage piece. It does not, in particular,
+implement any networking.
+*/
+
+package taskstore
+
+import (
+	"fmt"
+	"time"
+)
+
+// Task is the atomic task unit. It contains a unique task, an owner ID, and an
+// Available Time (AT). The data is user-defined and can be basically anything.
+//
+// 0 is an invalid ID, and is used to indicate "please assign".
+// A negative AT means "delete this task".
+type Task struct {
+	ID    int64
+	Owner int64
+	Group string
+
+	AT int64
+
+	Data interface{}
+}
+
+func (t *Task) String() string {
+	return fmt.Sprintf("Task %d: group=%s owner=%d at=%d data=%#v", t.ID, t.Group, t.Owner, t.AT, t.Data)
+}
+
+type taskStore struct {
+	journalDir string
+	groups     map[string]TaskQueue
+}
+
+func New(journalDir string) *taskStore {
+	// TODO: ensure that the directory either exists or can be created.
+	return &taskStore{
+		journalDir: journalDir,
+	}
+}
+
+func (t *taskStore) Init() {
+	// TODO: check that it is not already initialized.
+	//
+	// Create a single task in the empty group with the last usable task ID in it (initially 0).
+}
+
+func (t *taskStore) Start() {
+	// TODO: ensure that there can only be one instance of this thing.
+	// How do we do that? Using the file system? Some OS-level thing?
+	// TODO
+	// Look for the journal directory and replay any files there.
+}
+
+// getTask returns the task in the given group with the given ID.
+func (t *taskStore) getTask(group string, id int64) (task *Task, err error) {
+	g, ok := t.groups[group]
+	if !ok {
+		return nil, fmt.Errorf("No such group %q in taskstore", group)
+	}
+	task = g.PeekById(id)
+	if task == nil {
+		return nil, fmt.Errorf("No such task %d in group %q in taskstore", id, group)
+	}
+	return task, nil
+}
+
+func (t *taskStore) nowMicroseconds() int64 {
+	return time.Now().UnixNano() / 1000
+}
+
+// Update updates the tasks specified in the tasks list. If the AT for any task
+// is negative, it indicates that the task should be deleted.
+// In the event of success, the new IDs are returned. If the operation was not
+// successful, the list of errors will contain at least one non-nil entry.
+func (t *taskStore) Update(tasks []Task) (newIds []int64, errors []error) {
+	newIds = make([]int64, len(tasks))
+	errors = make([]error, len(tasks))
+	oldTasks := make([]*Task, len(tasks))
+
+	hasErrors := false
+
+	now := t.nowMicroseconds()
+
+	for i, task := range tasks {
+		if task.ID == 0 {
+			continue // just adding a new task
+		}
+		ot, err := t.getTask(task.Group, task.ID)
+		errors[i] = err
+		oldTasks[i] = ot
+		if errors[i] == nil {
+			if ot.AT > now && ot.Owner != task.Owner {
+				errors[i] = fmt.Errorf("Task %d in group %q is owned by %d, but update request came from %d", ot.ID, ot.Group, ot.Owner, task.Owner)
+			}
+		} else {
+			hasErrors = true
+		}
+	}
+
+	if hasErrors {
+		return newIds, errors
+	}
+
+	// If we got this far, then we're good to go. We can update all of the
+	// tasks with new IDs and new content. If AT is zero, we set it to now().
+	// If it is negative, we delete the task.
+	for _, task := range tasks {
+		g := t.groups[task.Group]
+		if task.AT == 0 {
+			task.AT = now
+		}
+		// TODO(chris): get a new ID
+		g.PopById(task.ID)
+		if task.AT >= 0 {
+			g.Push(&task)
+			// TODO(chris): send an update to the log
+		} else {
+			// TODO(chris): send a tombstone to the log
+		}
+	}
+	return newIds, nil
+}
